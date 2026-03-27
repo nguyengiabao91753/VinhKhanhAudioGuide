f576e410531472d788025e949a8af3bc5423d6db
cgbao2623 <chaugiabao02062003@gmail.com>
2026-03-27 10:49:15 +0700

    fix

--- Presentation/AudioTourApp/vite.config.ts
+++ Presentation/AudioTourApp/vite.config.ts
@@ -2,14 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,        
-    port: 5173,        
-    strictPort: true,  
-    allowedHosts: true 
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: true,
+
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5111',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   build: {
     rollupOptions: {
